279861eef1b407275c06307b3b9696894d67a883
Teddy Kagurazaka <ezkimili@gmail.com>
2019-01-16 14:30:21 +0800

    Initial Upload

---
+++ Generator.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+namespace FRSKeygen
+{
+    public static class Generator
+    {
+        static string customerName = "DUMMY";
+        static string issueDate = "20160101";
+        //static string key;
+        static string maximumCameras = "1024";
+        static string maximumEnrolmentSize = "20000";
+        static string maximumMatchingSize = "20000";
+
+        public static string Generate()
+        {
+            Console.WriteLine("Customer Name:" + customerName +
+                              "\nMax Camera:" + maximumCameras +
+                              "\nMax Enrollment:" + maximumEnrolmentSize +
+                              "\nMax MatchingSize:" + maximumMatchingSize +
+                              "\nIssue Date:" + issueDate + "\n");
+
+            MD5CryptoServiceProvider NameHasher = new MD5CryptoServiceProvider();
+            var NameHash = NameHasher.ComputeHash(System.Text.Encoding.UTF8.GetBytes(customerName));
+            string NameHashStr = "";
+            foreach (byte DetailByte in NameHash)
+                NameHashStr += DetailByte.ToString("X2");
+
+            NameHashStr = NameHashStr.Substring(8, 16);
+            Console.WriteLine("Name Hash:" + NameHashStr);
+
+            string MaxCameraHex = int.Parse(maximumCameras).ToString("X5");
+            string MaxEnrollHex = int.Parse(maximumEnrolmentSize).ToString("X8");
+            string MaxMatchHex = int.Parse(maximumMatchingSize).ToString("X8");
+            string DateHex = int.Parse(issueDate).ToString("X");
+            Console.WriteLine("Key Hash:" + MaxCameraHex + " " + MaxEnrollHex + " " + MaxMatchHex + " " + DateHex);
+
+            string OriginalCode = MaxCameraHex + MaxEnrollHex + MaxMatchHex + DateHex;
+            string SerialCode = "";
+            for (int i = 0; i < OriginalCode.Length;i++){
+                SerialCode += ToEncryptedCode(OriginalCode.Substring(i, 1));   
+            }
+            Console.WriteLine("Crypted Key:" +SerialCode);
+
+            //byte[] SerialCodeByte = Encoding.UTF8.GetBytes(SerialCode);
+            //byte[] NameByte = Encoding.UTF8.GetBytes(NameHashStr);
+            string FinalCode = "";
+
+            int SerialCodeCount = 0;
+            int NameByteCount = 0;
+
+            for (int q = 0; q < 44;q++){
+                if(q % 2 == 0 && q < 32)
+                {
+                    FinalCode += NameHashStr.Substring(NameByteCount, 1);
+                    NameByteCount++;
+                }else{
+                    FinalCode += SerialCode.Substring(SerialCodeCount, 1);
+                    SerialCodeCount++;
+                }
+            }
+
+            Console.WriteLine("Serial w/o checksum:" + FinalCode);
+            string Checksum = ReturnChecksum(FinalCode);
+            Console.WriteLine("Checksum:" + Checksum);
+            FinalCode = Checksum + FinalCode;
+
+            return FinalCode.Substring(0, 6) + "-" +
+                            FinalCode.Substring(6, 6) + "-" +
+                            FinalCode.Substring(12, 6) + "-" +
+                            FinalCode.Substring(18, 6) + "-" +
+                            FinalCode.Substring(24, 6) + "-" +
+                            FinalCode.Substring(30, 6) + "-" +
+                            FinalCode.Substring(36, 6) + "-" +
+                            FinalCode.Substring(42, 6);
+        }
+
+        public static string ReturnChecksum(string code){
+            int num2 = 0;
+            foreach (char ch in code.Replace("-", ""))
+            {
+                num2 += ch;
+            }
+            Console.WriteLine("Calculated Checksum:" + num2);
+            string OriginalChecksum = num2.ToString();
+            Console.WriteLine("Original Checksum:" + OriginalChecksum);
+            string EncryptedChecksum = "";
+            for (int i = 0; i < OriginalChecksum.Length; i++)
+            {
+                EncryptedChecksum += ToEncryptedCode(OriginalChecksum.Substring(i, 1));
+            }
+            return EncryptedChecksum;
+        }
+
+
+        private static string ToEncryptedCode(string code)
+        {
+            int index = 0;
+            string[] strArray2 = new string[] {
+                "S", "T", "U", "V", "W", "X", "Y", "Z", "Q", "R", "A", "B", "C", "D", "E", "F",
+                "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "2", "4", "9", "3", "8", "0",
+                "1", "5", "7", "6"
+             };
+            string[] strArray4 = new string[] {
+                "2", "4", "9", "3", "8", "0", "1", "5", "7", "6", "S", "T", "U", "V", "W", "X",
+                "Y", "Z", "Q", "R", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L",
+                "M", "N", "O", "P"
+             };
+            for (int i = 0; i < strArray2.Length; i++)
+            {
+                if (strArray4[i] == code)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return strArray2[index];
+        }
+    }
+
+    public class Configuration
+    {
+        // Fields
+        public const int CamerasLength = 5;
+        public const int EnrolmentSizeLength = 8;
+        public const int IssueDateLength = 7;
+        public const int KeyHeaderLength = 4;
+        public const int KeyLength = 0x37;
+        public const int KeySegmentCount = 8;
+        public const int MacLength = 0x10;
+        public const int MatchingSizeLength = 8;
+
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+
+namespace FRSKeygen
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string Serial = Generator.Generate();
+            Console.WriteLine(Serial);
+            //return;
+            Console.WriteLine("=============");
+
+            Console.WriteLine("Input:" + Serial);
+            Console.WriteLine("Checksum:" + (Checksum(Serial) ? "Passed" : "Failed"));
+            var Output = GetAuthorizationObj(Serial);
+            Console.WriteLine(Output.Output());
+        }
+
+        public static NeofaceLicense GetAuthorizationObj(string code)
+        {
+            NeofaceLicense license = null;
+            try
+            {
+                if (code.Length != 0x37)
+                {
+                    return null;
+                }
+                if (code.Split(new char[] { '-' }).Length == 0)
+                {
+                    return null;
+                }
+                string encoded = "";
+                foreach (string str3 in code.Substring(4).Split(new char[] { '-' }))
+                {
+                    //Console.WriteLine(encoded);
+                    encoded = encoded + str3;
+                }
+                Console.WriteLine(encoded);
+                string[] strArray2 = Decrypt(encoded).Split(new char[] { '-' });
+                if (strArray2.Length != 5)
+                {
+                    return null;
+                }
+                license = new NeofaceLicense (
+                    strArray2[0],
+                    strArray2[1],
+                    strArray2[2],
+                    strArray2[3],
+                    strArray2[4],
+                    code
+                );
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+                return null;
+            }
+            return license;
+        }
+
+        public static string Decrypt(string encoded)
+        {
+            char[] chArray = encoded.Replace("-", "").ToCharArray();
+            string str = "";
+            string code = "";
+            string str3 = "";
+            for (int i = 0; i < chArray.Length; i++)
+            {
+                if ((i > 0) && (i < 0x20))
+                {
+                    if ((i % 2) == 1)
+                    {
+                        code = code + chArray[i].ToString();
+                    }
+                    if ((i % 2) == 0)
+                    {
+                        str = str + chArray[i].ToString();
+                    }
+                }
+                if (i == 0)
+                {
+                    str = str + chArray[i].ToString();
+                }
+                if (i > 0x1f)
+                {
+                    code = code + chArray[i].ToString();
+                }
+            }
+            Console.WriteLine("str:" + str + ",code:" + code);
+            str3 = CharacterToInteger(code);
+            Console.WriteLine("decrypted code:" + str3);
+            new Configuration();
+            //
+            string str4 = int.Parse(str3.Substring(0, 5), System.Globalization.NumberStyles.HexNumber).ToString();
+            string str5 = int.Parse(str3.Substring(5, 8), System.Globalization.NumberStyles.HexNumber).ToString();
+            string str6 = int.Parse(str3.Substring(13, 8), System.Globalization.NumberStyles.HexNumber).ToString();
+            string str7 = int.Parse(str3.Substring(21, 7), System.Globalization.NumberStyles.HexNumber).ToString();
+            //Name:str
+            //Max Camera:str4
+            //Max Enrollment:str5
+            //Max MatchingSize:str6
+            //Issue Date:str7
+            return (str + "-" + str4 + "-" + str5 + "-" + str6 + "-" + str7);
+        }
+
+        private static string CharacterToInteger(string code)
+        {
+            string str = "";
+            foreach (char ch in code.ToCharArray())
+            {
+                str = str + ToOriginalCode(ch.ToString());
+            }
+            return str;
+        }
+
+        private static string ToOriginalCode(string code)
+        {
+            int index = 0;
+            string[] strArray2 = new string[] { 
+                "S", "T", "U", "V", "W", "X", "Y", "Z", "Q", "R", "A", "B", "C", "D", "E", "F", 
+                "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "2", "4", "9", "3", "8", "0", 
+                "1", "5", "7", "6"
+             };
+            string[] strArray4 = new string[] { 
+                "2", "4", "9", "3", "8", "0", "1", "5", "7", "6", "S", "T", "U", "V", "W", "X", 
+                "Y", "Z", "Q", "R", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
+                "M", "N", "O", "P"
+             };
+            for (int i = 0; i < strArray2.Length; i++)
+            {
+                if (strArray2[i] == code)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return strArray4[index];
+        }
+ 
+        public static bool Checksum(string code){
+            string[] strArray = code.Split(new char[] { '-' });
+            if (strArray.Length != 8)
+            {
+                return false;
+            }
+
+            string s = CharacterToInteger(strArray[0].Substring(0, 4));
+            int result = 0;
+            if (!int.TryParse(s, out result))
+            {
+                return false;
+            }
+            int num2 = 0;
+            foreach (char ch in code.Replace("-", "").Substring(4).ToCharArray())
+            {
+                num2 += ch;
+            }
+            Console.WriteLine("Desired result:" + result + ",Calculated:" + num2);
+            if (result != num2)
+            {
+                
+                return false;
+            }
+            return true;
+
+        }
+    }
+
+    public class NeofaceLicense
+    {
+        // Fields
+        private string customerName;
+        private string issueDate;
+        private string key;
+        private string maximumCameras;
+        private string maximumEnrolmentSize;
+        private string maximumMatchingSize;
+        private string trailDays;
+
+        public NeofaceLicense(string CustName,string MaxCamera,string MaxEnrolSize,string MaxMatchingSize,string IssDate,string InputKey){
+            customerName = CustName;
+            maximumCameras = MaxCamera;
+            maximumEnrolmentSize = MaxEnrolSize;
+            maximumMatchingSize = MaxMatchingSize;
+            issueDate = IssDate;
+            key = InputKey;
+        }
+
+        public string Output()
+        {
+            return "Customer Name(In MD5):" + customerName + "\nMax Camera:" + maximumCameras + "\nMax Enrollment:" + maximumEnrolmentSize + "\nMax MatchingSize:" + maximumMatchingSize + "\nIssue Date:" + issueDate + "\nSerial:" + key;
+        }
+    }
+
+
+
+ 
+
+}
